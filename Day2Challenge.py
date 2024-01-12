8c39bfa9ae8a0efc5201e875d7cd6ff0245761af
mj117 <mj117@iu.edu>
2024-01-12 00:34:11 -0500

    Updating the code to get the correct output

--- Day2Challenge.py
+++ Day2Challenge.py
@@ -6,12 +6,14 @@ def areaOfCircle(rad):
         return (pi * pow(rad,2))
     except ValueError:
         print("Please input only numerical values. Please try again!")
-        
+        return
+
 
 
 if __name__ == "__main__":
     rad = input("Please enter the radius of circle: ")
     area = areaOfCircle(rad)
-    print("Area of the Circle with the radius ", rad, " unit is: ", area, " square unit.")
+    if area != None:
+        print("Area of the Circle with the radius ", rad, " unit is: ", area, " square unit.")
     
     
